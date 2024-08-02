941fb602ae1404f87675d738589d7fe032054b0a
Marcoproxdd <145713431+Marcoproxdd@users.noreply.github.com>
2024-08-02 18:32:30 -0500

    Commit Final
    Error de subida de archivos
    
    Co-Authored-By: Annel Flores <106723247+Annel-Flores@users.noreply.github.com>
    Co-Authored-By: CRISTHINAPA <130621476+CRISTHINAPA@users.noreply.github.com>

--- front-rrhh-nodejs-main/src/app/components/stats-card/stats-card.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  standalone: true,
-  selector: 'app-stats-card',
-  templateUrl: './stats-card.component.html',
-  styleUrls: ['./stats-card.component.css']
-})
-export class StatsCardComponent { }
--- front-rrhh-nodejs-main/src/main.server.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { config } from './app/app-routing.module';
-
-const bootstrap = () => bootstrapApplication(AppComponent);
-
-console.log(config.apiUrl);
-
-export default bootstrap;
